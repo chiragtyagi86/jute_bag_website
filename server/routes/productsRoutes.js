8c47598baf6226eb4db1da61608ae408595d3008
CHIRAG TYAGI <69377702+chiragtyagi86@users.noreply.github.com>
2024-10-16 22:03:05 +0530

    user authentication

--- server/routes/productsRoutes.js
+++ server/routes/productsRoutes.js
@@ -8,24 +8,13 @@ const cors = require('cors');
 require('dotenv').config();
 const secretKey = process.env.JWT_SECRET;
 const db = require("../config/db");
-const ensureAuthenticated = require('../middleware/AuthenticateUserToken');
 
 
 
 router.use(express.json());
 router.use(cors());
 
-router.get("/",ensureAuthenticated,(req, res) => {
-    console.log('Logging user details:',req.user);
-    res.status(200).json([{
-        name:"bag1",
-        price:'1000'
-    },{
-        name:"bag1",
-        price:'1000'
-    }]);
-    // res.sendFile(path.join(__dirname, '..', '..', 'html', 'product.html'));
-});
+
 
 
 
