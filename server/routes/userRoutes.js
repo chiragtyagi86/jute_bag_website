f84d30309d9aef247fd5b85f347180e75c6a2368
CHIRAG TYAGI <69377702+chiragtyagi86@users.noreply.github.com>
2024-10-29 22:35:26 +0530

    update errors in all routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -13,10 +13,9 @@ router.get("/", function (req, res) {
 });
 
 const razorpay = new Razorpay({
-  key_id: process.env.key_id, 
-  key_secret: process.env.key_secret, 
+  key_id: process.env.key_id,
+  key_secret: process.env.key_secret,
 });
-
 
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
 let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
@@ -114,7 +113,7 @@ router.post("/sign-in", (req, res) => {
     if (results.length === 0) {
       return res.status(401).json({ error: "Invalid credentials" });
     }
-    
+
     const user = results[0]; // Correctly define user
 
     bcrypt.compare(password, user.password, (err, isMatch) => {
@@ -164,10 +163,11 @@ router.get("/dashboard", authenticateToken, (req, res) => {
 function getOrderId() {
   return "Order" + Math.floor(100000 + Math.random() * 900000);
 }
-router.post("/buy-products", authenticateToken, (req, res) => {
-  const {product_id, email, payment_method, price, product_qty} = req.body;
 
-  if (!product_id ||!email ||!payment_method ||!product_qty || !price) {
+router.post("/buy-products", authenticateToken, (req, res) => {
+  const { product_id, email, payment_method, price, product_qty } = req.body;
+
+  if (!product_id || !email || !payment_method || !product_qty || !price) {
     return res.status(400).json({ error: "All fields are required" });
   }
   const order = {
@@ -177,27 +177,41 @@ router.post("/buy-products", authenticateToken, (req, res) => {
     payment_method,
     product_qty,
     price,
-  }
-
-  const sql = "INSERT INTO orders SET?"
+  };
+  const getQty = "SELECT product_qty FROM product_data WHERE product_id = ?";
+  const updateProduct = `
+     UPDATE product_data 
+  SET product_qty = ?, 
+      product_sales = product_sales + 1 
+  WHERE product_id = ?
+     `;
+  const sql = "INSERT INTO orders SET?";
   db.query(sql, order, (err, result) => {
     if (err) {
       console.error("Failed to insert order:", err);
       return res.status(500).json({ error: "Failed to insert order" });
     }
-    res.json({
-      message: "Order inserted successfully",
-      id: result.insertId,
-    })
-  })
-} )
+    db.query(getQty, [product_id], (err, result) => {
+      if (err) return res.status(500).json({ message: "Server error" });
+      const remaining_qty = result[0].product_qty - product_qty;
+      db.query(updateProduct, [remaining_qty, product_id], (err, result) => {
+        if (err)
+          return res
+            .status(500)
+            .json({ error: "Failed to update product quantity" });
+        res.json({
+          message: "Order inserted successfully",
+          id: result.insertId,
+        });
+      });
+    });
+  });
+});
 
-//show orders
-router.get("/orders/:email",  (req, res) => {
-  const {email} = req.params;    
-  const sql = 'SELECT * FROM `orders`  WHERE email =? ORDER BY `orders`.`order_added` DESC;'
-
-  
+router.get("/orders/:email", (req, res) => {
+  const { email } = req.params;
+  const sql =
+    "SELECT * FROM `orders`  WHERE email =? ORDER BY `orders`.`order_added` DESC;";
 
   db.query(sql, [email], (err, result) => {
     if (err) return res.status(500).json({ message: "Server error" });
@@ -228,14 +242,14 @@ router.post("/payment", authenticateToken, (req, res) => {
     }
     res.json(order);
     console.log(order);
-     // Send order details back to client
+    // Send order details back to client
   });
 });
 
 router.post("/create-order", authenticateToken, (req, res) => {
-  const {product_id, email, payment_method, price, product_qty} = req.body;
-  
-  if (!product_id ||!email ||!payment_method ||!product_qty || !price) {
+  const { product_id, email, payment_method, price, product_qty } = req.body;
+
+  if (!product_id || !email || !payment_method || !product_qty || !price) {
     return res.status(400).json({ error: "All fields are required" });
   }
   const options = {
@@ -267,7 +281,5 @@ router.post("/payment/verify", (req, res) => {
     res.status(400).json({ error: "Payment verification failed" });
   }
 });
-
-
 
 module.exports = router;
